69c98fd11191930e7277b0f1a06b63444fcf7728
ST10355869 <128582083+ST10355869@users.noreply.github.com>
2024-10-01 16:35:07 +0200

    Add project files.

---
+++ ProcessQueueMessage.cs
@@ -0,0 +1,27 @@
+using Azure.Storage.Queues;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+public static class ProcessQueueMessage
+{
+    [Function("ProcessQueueMessage")]
+    public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+        ILogger log)
+    {
+        var connectionString = Environment.GetEnvironmentVariable("AzureStorage:ConnectionString");
+        string queueName = req.Query["queueName"];
+        string message = req.Query["message"];
+
+
+        var queueServiceClient = new QueueServiceClient(connectionString);
+        var queueClient = queueServiceClient.GetQueueClient(queueName);
+        await queueClient.CreateIfNotExistsAsync();
+        await queueClient.SendMessageAsync(message);
+
+        return new OkObjectResult("Message added to queue");
+    }
+}
